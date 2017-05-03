3e93efc283d9edd1a904d1af5fe39a0d9207084f
MolecularDeveloper <smadsen@beveragefactory.com>
2017-05-03 15:04:54 -0700

    history and router

--- modules/App.js
+++ modules/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavLink from './NavLink'
 import Home from './Home'
+import { IndexLink } from 'react-router'
 
 export default React.createClass({
   render() {
@@ -8,6 +9,7 @@ export default React.createClass({
       <div>
       	<h6>react router </h6>
       	<ul role="nav">
+      		<li><IndexLink to="/" activeClassName="active">home</IndexLink></li>
       	  <li><NavLink to="/about/" activeClassName="active"> about</NavLink></li>
       	  <li><NavLink to="/repos/" activeClassName="active"> repos</NavLink></li>
       	 </ul>
